7729a0195470676d59f10183f82b05d78f71b97f
Karol-Waliszewski <karolwaliszewski@gmail.com>
2022-07-22 15:15:13 +0200

    Moved action logic to actions.ts

--- src/store/game/actions.ts
+++ src/store/game/actions.ts
@@ -1,5 +1,10 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, ActionReducerMapBuilder } from "@reduxjs/toolkit";
+import initialState, { GameState } from "store/game/state";
 import { Cell, Move, GameStatus } from "types/game";
+
+import { findAllPossibleMoves } from "utils/game/board/moving";
+import { areCellsSame } from "utils/game/board/functional";
+import { makeMove, startGame as startGameEngine } from "utils/game/engine";
 
 export const resetGame = createAction("game/resetGame");
 export const startGame = createAction("game/startGame");
@@ -8,3 +13,39 @@ export const toggleCell = createAction<Cell>("game/toggleCell");
 export const movePiece = createAction<{ move: Move; decisionTime: number }>(
   "game/movePiece"
 );
+
+export const actionBuilder = (builder: ActionReducerMapBuilder<GameState>) => {
+  builder
+    .addCase(resetGame, () => ({ ...initialState }))
+    .addCase(startGame, (state) => ({
+      ...state,
+      ...startGameEngine(state),
+    }))
+    .addCase(setStatus, (state, action) => {
+      state.status = action.payload;
+    })
+    .addCase(toggleCell, (state, action) => {
+      const isAlreadyToggled =
+        state.toggledCell && areCellsSame(action.payload, state.toggledCell);
+      state.toggledCell = isAlreadyToggled ? null : action.payload;
+    })
+    .addCase(movePiece, (state, action) => {
+      const gameState = makeMove(
+        state,
+        action.payload.move,
+        action.payload.decisionTime
+      );
+
+      return {
+        ...gameState,
+        toggledCell: null,
+        possibleMoves:
+          gameState.currentPlayer.type === "human"
+            ? findAllPossibleMoves(
+                gameState.board.grid,
+                gameState.currentPlayer.color
+              )
+            : [],
+      };
+    })
+};
--- src/store/game/reducers.ts
+++ src/store/game/reducers.ts
@@ -1,47 +1,9 @@
 import { createReducer } from "@reduxjs/toolkit";
 import initialState from "store/game/state";
 import {
-  movePiece as movePieceAction,
-  resetGame as resetGameAction,
-  startGame as startGameAction,
-  setStatus,
-  toggleCell,
+  actionBuilder,
 } from "store/game/actions";
-import { areCellsSame } from "utils/game/board/functional";
-import { findAllPossibleMoves } from "utils/game/board/moving";
-import { makeMove, startGame } from "utils/game/engine";
 
-const gameReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(resetGameAction, (state) => ({ ...initialState }))
-    .addCase(startGameAction, (state) => ({
-      toggledCell: null,
-      possibleMoves: [],
-      ...startGame(state),
-    }))
-    .addCase(setStatus, (state, action) => {
-      state.status = action.payload;
-    })
-    .addCase(toggleCell, (state, action) => {
-      const isAlreadyToggled =
-        state.toggledCell && areCellsSame(action.payload, state.toggledCell);
-      state.toggledCell = isAlreadyToggled ? null : action.payload;
-    })
-    .addCase(movePieceAction, (state, action) => {
-      const gameState = makeMove(state, action.payload.move, action.payload.decisionTime);
-
-      return {
-        ...gameState,
-        toggledCell: null,
-        possibleMoves:
-          gameState.currentPlayer.type === "human"
-            ? findAllPossibleMoves(
-                gameState.board.grid,
-                gameState.currentPlayer.color
-              )
-            : [],
-      };
-    });
-});
+const gameReducer = createReducer(initialState, actionBuilder);
 
 export default gameReducer;
